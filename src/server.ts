0f29c2e17c4e7f98138831ff632949e33f7f4038
AyushR1 <ayushrathore12501@gmail.com>
2024-12-12 19:48:11 +0530

    Init Backend
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import { createServer } from "http";
+import { WebSocketServer } from "ws";
+import { setupWebSocket } from "./websocket";
+
+// Express server
+const app = express();
+const port = 3000;
+
+// Serve static files
+app.use(express.static("public"));
+
+// Create an HTTP server
+const server = createServer(app);
+
+// Initialize WebSocket server
+setupWebSocket(server);
+
+// Start server
+server.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
